75b2fe06e534d12d2dd86a7254a11cf0b5564b1a
AntuVenciu <antoinev151@gmail.com>
2023-06-08 11:49:20 +0200

    Add plotting feature

--- residuals_analysis.py
+++ residuals_analysis.py
@@ -3,6 +3,7 @@ Draw residuals from cosmic track fit at each millepede iteration
 """
 import os
 import glob
+import ROOT
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,13 +19,19 @@ directory = f"residuals_iter_{ITERATION}"
 wires = np.array([])
 dres = np.array([])
 zvec = np.array([])
+xres = np.array([])
+yres = np.array([])
 
 # Loop over all files res_437*.txt in the directory
-for res_file in glob.iglob(f"{directory}/res_4*.txt"):
-    doca, res, sigma, x, y, z, wire = np.loadtxt(res_file, unpack=True)
+for res_file in glob.iglob(f"{directory}/res_*iter0.txt"):
+    if os.path.getsize(res_file) < 1000:
+        continue
+    doca, res, sigma, z, x, y, wire = np.loadtxt(res_file, unpack=True)
     wires = np.append(wires, wire)
     dres = np.append(dres, res)
-    zvec.append(zvec, z)
+    zvec = np.append(zvec, z)
+    xres = np.append(xres, x)
+    yres = np.append(yres, y)
 
 if WRITE_WIRE_LIST:
     # Count hits on wire to establish good wires (with > 50 hits) to be aligned
@@ -44,27 +51,68 @@ for i in range(1, 10, 1):
     plt.xlim(0, 192)
     plt.ylim(-0.1, 0.1)
     plt.hist2d(wires[wires%10 == i]%192, dres[wires%10 == i], bins=[192, 100], range=[[-0.5, 192.5], [-0.1, 0.1]])
+plt.savefig(f"residuals_plane_per_plane_iter{ITERATION}.pdf")
 
-# Histogram of residuals VS z for four "average" wires
-plt.figure(2)
 wire_list = np.zeros(1920)
 for w in wires:
     wire_list[int(w)] += 1
 mean = np.mean(wire_list)
 wires_plot = []
+
 found = 0
+#hxresz = ROOT.TH2F("hxresz", "x Res VS z (#gamma determination); z [cm]; x Res [cm]", 40, -80, 80, 40, -0.2, 0.2)
 for w in wires:
-    if found = 4:
+    if found == 1:
         break
-    if abs(wire_list[int(w)] - mean) < 5:
+    if abs(wire_list[int(w)] - mean) < np.sqrt(mean):
         wires_plot.append(w)
         found += 1
+
+# ROOOT CODE
+"""
+for x, z, w in zip(xres, zvec, wires):
+    if w != wires_plot[0]:
+        continue
+    #print(f"Filling with {x} {z}")
+    hxresz.Fill(z, x)
+
+c = ROOT.TCanvas("c")
+hxresz.Draw("colz")
+prof = hxresz.ProfileX("pfx", 1, -1, "dsame")
+prof.SetMarkerStyle(20)
+prof.SetMarkerSize(1)
+c.SaveAs("test.pdf")
+"""
+
+# Histogram of residuals VS z for four "average" wires
+plt.figure(2)
+plt.title(r"$\gamma$ from y Res VS z")
 for i, w in enumerate(wires_plot):
     plt.subplot(2, 2, i + 1)
-    plt.ylabel("doca Res [cm]")
+    plt.ylabel("y Res [cm]")
     plt.xlabel("z [cm]")
     plt.ylim(-0.2, 0.2)
-    plt.xlim(-90, 90)
-    plt.hist2d(dres[wires==w], zvec[wires==w], bins=[50, 100], range=[[-90, 90], [-0.2, 0.2]])
+    plt.xlim(-80, 80)
+    plt.hist2d(zvec[wires==w], yres[wires==w], bins=[50, 25], range=[[-80, 80], [-0.2 , 0.2]])
+plt.savefig(f"gamma_yres_z_iter{ITERATION}.pdf")
+    
+plt.figure(3)
+plt.title(r"$\gamma$ from x Res VS z")
+for i, w in enumerate(wires_plot):
+    plt.subplot(2, 2, i + 1)
+    plt.ylabel("x Res [cm]")
+    plt.xlabel("z [cm]")
+    plt.ylim(-0.2, 0.2)
+    plt.xlim(-80, 80)
+    
+    plt.hist2d(zvec[wires==w], xres[wires==w], bins=[50, 25], range=[[-80, 80], [-0.2 , 0.2]])
+plt.savefig(f"gamma_xres_z_iter{ITERATION}.pdf")
+    
+plt.figure(4)
+plt.title("DOCA Residuals")
+plt.xlabel("$d_{hit} - d_{track}$ [cm]")
+plt.hist(dres, bins=100, range=(-0.15, 0.15))
+plt.savefig(f"doca_res_iter{ITERATION}.pdf")
 
 plt.show()
+
